0d5d9b0b8116fc8d4bb3a97c35769c2bfd5bf24f
jamie <jamie927@gmail.com>
2026-07-16 19:06:13 -0700

    working detail and update

--- backend/routes/performanceRoutes.ts
+++ backend/routes/performanceRoutes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { pool } from "../db/pool.ts";
 import { getPerformances } from "../models/performanceModel.ts";
 
 const router = express.Router();
@@ -14,6 +15,34 @@ router.get("/", async (req, res) => {
 
   const players = await getPerformances(limit, offset, search);
   res.json(players);
+});
+
+router.get("/player/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    
+    const result = await pool.query("SELECT * FROM players WHERE player_id = $1", [id]);
+    
+    if (result.rows.length > 0) {
+      res.json(result.rows[0]);
+    } else {
+      res.status(404).json({ error: "Player not found" });
+    }
+  } catch (err) {
+    console.error("DB Error:", err);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+// Update
+router.put("/player/:id", async (req, res) => {
+  const { id } = req.params;
+  const { player_name, team, position } = req.body;
+  await pool.query(
+    "UPDATE players SET player_name = $1, team = $2, position = $3 WHERE player_id = $4",
+    [player_name, team, position, id]
+  );
+  res.json({ message: "Updated successfully" });
 });
 
 export default router;
